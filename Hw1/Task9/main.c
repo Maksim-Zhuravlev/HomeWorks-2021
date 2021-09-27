dcbea75577715d6214822868ee25e22d433372c7
Alexandr Zabelin <alexandre.zabelin@gmail.com>
2021-09-27 20:21:30 +0300

    Completed Task 7

--- Hw1/Task9/main.c
+++ Hw1/Task9/main.c
@@ -3,6 +3,21 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    char s1[] = "12", s[] = "123454212";
+    int k = 0, size_Of_S = sizeof(s) / sizeof(s[0]), size_Of_S1 = sizeof(s1) / sizeof(s1[0]);
+    printf("S: %d\nS1: %d", size_Of_S, size_Of_S1);
+    for (int i = 0; i <= size_Of_S - size_Of_S1; i++) {
+        if (s[i] == s1[0]) {
+            int ok = 1;
+            for(j = 1; j < size_Of_S1; j++) {
+                if (s1[j] != s[i + j]) {
+                    ok = 0;
+                    break;
+                }
+            }
+            k += ok
+        }
+    }
+    printf("k = %d", k);
     return 0;
 }
