c92f856c480bc8b5af6548e7d1ed81d704898f74
Nikita-Oskin <91139480+Nikita-Oskin@users.noreply.github.com>
2021-10-26 13:23:32 +0300

    Add files via upload

---
+++ StrxxxOskin.c
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+// int div task-2
+int divT(int divisor, int divident)
+{
+	int result = 0;
+	if (divisor < 0 && divident > 0)
+	{
+		int ost = divisor + divident;
+		for (; ost <= 0; ost += divident)
+			result--;
+		return ost ? result - 1 : result;
+	}
+	else
+	{
+		for (int i = divisor - abs(divident); i >= 0; i -= abs(divident))
+			result++;
+		return divident > 0 ? result : -result;
+	}
+}
+
+int main()
+{
+	int A, B;
+
+	printf("Enter A:\n");
+	scanf("%d", &A);
+	printf("Enter B:\n");
+	scanf("%d", &B);
+
+	printf("\n%d", divT(A, B));
+
+}
+
