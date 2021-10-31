de008b5aec9efccc08b4446e93e770d600c0eb6f
PolinaBadreeva <91344113+PolinaBadreeva@users.noreply.github.com>
2021-10-31 19:40:50 +0300

    Tasks from a introductory test
    badreeva_hw_1 Tasks from a introductory test

---
+++ Hw1/(8).c
@@ -0,0 +1,22 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int main()
+{
+	int q, e, t = 10, j;
+	scanf("%d", &q);
+	if (q > 1) printf("1 ");
+	for (int i = 2; i <= q; i++)
+	{
+		e = i; t = 10;
+		for (j = 2; j < e; j++)
+		{
+			if (e % j == 0)
+			{
+				t = 0;
+				break;
+			}
+		}
+		if (t == 10) printf("%d ", j);
+	}
+}
---
+++ Hw1/(9).c
@@ -0,0 +1,21 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include< string.h>
+int main() {
+    char str[100], q[100];
+    int i = 0, e = 1, kolvo = 0, c = 0;
+    scanf("%s%s", &str, &q);
+    while (q[c] != '\0') {
+        c++;
+    }
+    while (str[i - c] != '\0') {
+        e = 1;
+        for (int w = 0; w < c; w++)
+        {
+            if (q[w] != str[i + w]) e = 100;
+        }
+        if (e == 1) kolvo++;
+        i++;
+    }
+    printf("%d", kolvo);
+}
