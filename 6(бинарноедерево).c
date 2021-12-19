0e5bb0d026d864cf9d2f70f00101ad0345020c49
PolinaBadreeva <91344113+PolinaBadreeva@users.noreply.github.com>
2021-12-19 14:46:04 +0300

    tree
    tree

--- 6(бинарноедерево).c
+++ 6(бинарноедерево).c
@@ -9,25 +9,32 @@
 #include <ctype.h>
 #define MAXWORD 100
 
-struct tnode {           
-	char* word;          
-	int count;           
-	struct tnode* left;  
-	struct tnode* right; 
+struct tnode {
+	char* word;
+	int count;
+	struct tnode* left;
+	struct tnode* right;
 };
-char* strdup(char* s) 
+char* strdup(char* s)
 {
 	char* p;
-
-	p = (char*)malloc(strlen(s) + 1); 
-	if (p != NULL)
+	p = (char*)malloc(strlen(s) + 1);
+	if (p == NULL) {
+		printf("error");
+	}
+	else
+	{
 		strcpy(p, s);
-	return p;
+		return p;
+	}
 }
-struct tnode* addtree(struct tnode* p, char* w) {
+struct tnode* add_tree(struct tnode* p, char* w) {
 	int cond;
 	if (p == NULL) {
 		p = (struct tnode*)malloc(sizeof(struct tnode));
+		if (!p) {
+			printf("error");
+		}
 		p->word = strdup(w);
 		p->count = 1;
 		p->left = p->right = NULL;
@@ -35,30 +42,79 @@ struct tnode* addtree(struct tnode* p, char* w) {
 	else if ((cond = strcmp(w, p->word)) == 0)
 		p->count++;
 	else if (cond < 0)
-		p->left = addtree(p->left, w);
+		p->left = add_tree(p->left, w);
 	else
-		p->right = addtree(p->right, w);
+		p->right = add_tree(p->right, w);
 	return p;
 }
 
-void treeprint(struct tnode* p) {
+void tree_print(struct tnode* p) {
+	int e = 0;
 	if (p != NULL) {
-		treeprint(p->left);
+		tree_print(p->left);
 		printf("%d %s\n", p->count, p->word);
-		treeprint(p->right);
+		tree_print(p->right);
 	}
 }
-int Height(struct tnode* p) {
+void free_tree(struct tnode* p) {
+	if (p->left)   free_tree(p->left);
+	if (p->right)  free_tree(p->right);
+	free(p);
+}
+int height(struct tnode* p) {
 	int l, r, h = 0;
 	if (p != NULL) {
-		l = Height(p->left);
-		r = Height(p->right);
+		l = height(p->left);
+		r = height(p->right);
 		h = ((l > r) ? l : r) + 1;
 	}
 	return h;
 }
+int loong(struct tnode* p) {
+	int e = 0;
+	if (p != NULL)
+	{
+		e = p->count;
+		if (p->right != NULL) {
+			if (loong(p->right) >= e)
+			{
+				e = loong(p->right);
+			}
+		}
+	}
+	if (p->left != NULL) {
+		if (loong(p->left) >= e)
+		{
+			e = loong(p->left);
+		}
+	}
+	return e;
+}
+
+void word(struct tnode* p, int t) {
+	if (p != NULL)
+	{
+		word(p->left, t);
+		if (p->left != NULL) {
+			if (p->count == t) {
+				printf("%s \n", p->word);
+				t++;
+
+			}
+		}
+		word(p->right, t);
+		if (p->right != NULL) {
+			if (p->count == t) {
+				printf("%s \n", p->word);
+				t++;
+			}
+		}
+	}
+}
+
+
 int main() {
-	int i = -1, w = 0;
+	int i = -1, w = 0, t;
 	struct tnode* root;
 	char data[30];
 	root = NULL;
@@ -73,10 +129,14 @@ int main() {
 		i++;
 		fscanf(file, "%s", data);
 		if (isalpha(data[0]))
-			root = addtree(root, data);
+			root = add_tree(root, data);
 		strcpy(data, "");
 	}
 	fclose(file);
-	printf("%d \n", Height(root));
-	treeprint(root);
+	printf("%d \n", height(root));
+	t = loong(root);
+	word(root, t);
+	printf("%d \n", t);
+	tree_print(root);
+	free_tree(root);
 }
