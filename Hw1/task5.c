5b6242dde96ad4f660c7731ad0adcab4e22fed58
DmitryPilyuk <st095257@student.spbu.ru>
2021-12-19 16:17:21 +0300

    Сorrection of mistakes

--- Hw1/task5.c
+++ Hw1/task5.c
@@ -1,19 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+long pow(int n, int deg) {
+
+	long res = 1;
+
+	while (deg) {
+		if (deg % 2 != 0) {
+			deg--;
+			res *= n;
+		}
+		else {
+			deg /= 2;
+			n *= n;
+		}
+	}
+}
+
 int main()
 {
-	int a;
 	int n;
- 	int ans=1;
+	int deg;
+ 	int ans = 1;
+
 	printf("Enter number:");
-	scanf("%d", &a);
-	printf("Enter the degree:");
 	scanf("%d", &n);
-	for(int i=1; i<=n; i++)
-	{
-		ans *= a;
-	}
-	printf("Answer: %d",ans);
+	printf("Enter the degree:");
+	scanf("%d", &deg);
+
+	printf("Answer: %d",pow(n, deg);
 	return 0;
 }
--- Hw1/task6.c
+++ Hw1/task6.c
@@ -1,8 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main()
-{
+int main() {
 	int key = 1;
 	int n;
 	int count = 0;
@@ -10,28 +9,25 @@ int main()
 	printf("Enter the number of multipliers\n");
 	scanf("%d", &n);
 	printf("Enter %d multipliers\n",n);
-	for (int i=1; i<=n; i++)
-	{
+	for (int i=1; i<=n; i++) {
 		scanf("%d", &x);
-		if (x < 0)
-		{
+		if (x < 0) {
 			count += 1;
 		}
-		if (x == 0)
-		{
+		if (x == 0) {
 			key = 0;
 		}
 	}
-	if (key > 0)
-	{
-		if(count%2 == 0)
-		{
+	if (key > 0) {
+		if (count%2 == 0) {
 			printf("The sign is +");
 		}
-		else
+		else {
 			printf("The sign is -");
+		}
 	}
-	else
+	else {
 		printf("The sign is 0");
+	}
 	return 0;
 }
