0593e1d015661bb8212aeee7be30d142c802934f
Valerii Tinkevich <59338294+ValeriiTinkevich@users.noreply.github.com>
2021-12-29 02:24:04 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,78 +1,33 @@
 #include <stdio.h>
 
-#define MAX_LEN 256
-
-int strlen(const char *Str) {
-    if(Str == NULL)
-    {
-        return -1;
-    }
-    int i = 0;
-    while (Str[i] != '\0')
-    {
-        i++;
-    }
-    return i;
-}
-
-int strcmp(const char *str1, const char *str2) {
-    if (str1 == NULL && str2 != NULL) {
-        return -1;
-    } else if (str2 == NULL && str1 != NULL) {
-        return 1;
-    } else if (str1 == NULL && str2 == NULL){
+int div(int divisor, int divident) {
+    if (divisor == 0) {
         return 0;
     }
-
-    int i = 0;
-    while (*(str1 + i) == *(str2 + i) && *(str1 + i) != '\0')
-    {
-        i++;
+    int g = 0;
+    int flag = 0;
+    if (divisor < 0 && divident < 0) {
+        flag = 0;
+        divisor = -divisor;
+        divident = -divident;
+    } else if (divisor > 0 && divident < 0) {
+        flag = 1;
+        divident = -divident;
+    } else if (divisor < 0 && divident > 0) {
+        flag = 1;
+        divisor = -divisor;
     }
-    if (*(str1 + i) > *(str2 + i))
-    {
-        return 1;
+    while (divisor >= divident) {
+        divisor = divisor - divident;
+        g++;
     }
-    else if (*(str1 + i) < *(str2 + i))
-    {
-        return -1;
+    if (flag == 1) {
+        g = -g;
     }
-    else
-    {
-        return 0;
-    }
-}
-
-char *strcpy(char * str1, const char * str2 ) {
-    if(str1 == NULL || str2 == NULL)
-    {
-        return NULL;
-    }
-    char *p = str1;
-    while((* str1++ = * str2++) != '\0');
-    return p;
-}
-
-char *strcat(char *dest, const char *src) {
-    char *rdest = dest;
-
-    while (*dest)
-        dest++;
-    while ((*dest++ = *src++))
-        ;
-    return rdest;
+    return g;
 }
 
 int main() {
-    char str1[MAX_LEN]; str1[0] = '\0';
-    char str2[MAX_LEN]; str1[0] = '\0';
-    scanf("%s", str1);
-    scanf("%s", str2);
-    int a = strcmp(str1, str2);
-    char g[10]={"abc"}, b[10]={"def"};
-    strcat(g,b);
-    printf("%s",g);
-    getchar();
-
+    printf("%d", div(0, -5));
     return 0;
 }
