811a18a3e1d1386b8de289ae5ce424a1abfdcbda
Nyamdavaa-Bishrel <91365132+Nyamdavaa-Bishrel@users.noreply.github.com>
2022-01-10 14:49:42 +0300

    fixed 8

--- 8.c
+++ 8.c
@@ -6,11 +6,10 @@ int main () {
    int a;
    printf("Enter a number:");
    scanf("%d",&a);
-   for(i = 2; i<a; i++) {
-
-      for(j = 2; j <= (i/j); j++) 
-      if(!(i%j)) break;
-      if(j > (i/j)) printf("%d is prime\n", i);
+   for(i = 2; i < a; i++) {
+      for(j = 2; j <= (i / j); j++) 
+      if(!(i % j)) break;
+      if(j > (i / j)) printf("%d is prime\n", i);
    }
  
    return 0;
